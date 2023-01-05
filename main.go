9c46889d8ad1398ec04d1d5cc41ad47f71ace82b
Mike Spreitzer <mspreitz@us.ibm.com>
2023-01-05 14:13:08 -0500

    Demonstrate bug involving capitalization

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "flag"
-import "k8s.io/klog/v2"
+import (
+	"flag"
+	"context"
+
+	"k8s.io/klog/v2"
+)
 
 func main() {
 	klog.InitFlags(flag.CommandLine)
+	flag.Parse()
+	ctx := context.Background()
+	logger := klog.FromContext(ctx)
+	logger.Info("My message", "Fubar", "baz")
+	logger.Info("My message", "fubar", "baz")
 }
